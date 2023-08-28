fa81e5e6b2df04326b2ffcdb8dd311d3b587ca72
Vinchy <empty>
2023-08-28 20:45:22 +0200

    updated the main file.

--- main.cpp
+++ main.cpp
@@ -1,16 +1,49 @@
 #include <SFML/Graphics.hpp>
+#include "Game.h"
+#include "MapGenerator.h"
+#include "Player.h"
 
 #include <iostream>
+#include <stdlib.h>
 
 int main()
 {
-    sf::RectangleShape player(sf::Vector2f(50.0f, 50.0f));
-    player.setFillColor(sf::Color::Cyan);
+    // Map generation
+    MapGenerator mapGen(150, 50, 50);
+    mapGen.GenerateMap();
+    mapGen.PrintMap();
+    int** map = mapGen.GetMap();
 
     sf::RenderWindow window(sf::VideoMode(512, 512), "Game", sf::Style::None);
 
+    Player player;
+    window.setView(player.GetView());
+
+    sf::Sprite** sprites;
+
+    sprites = new sf::Sprite*[mapGen.GetWidth()];
+    for (int i=0; i<mapGen.GetWidth(); i++)
+        sprites[i] = new sf::Sprite[mapGen.GetHeight()];
+
+    // Load textures
+    sf::Texture wall;
+    if (!wall.loadFromFile("res/textures/tile_stone.png"))
+    {
+        std::cout << "Failed to load stone tile texture.\n";
+        return -1;
+    }
+
+    sf::Texture path;
+    if (!path.loadFromFile("res/textures/tile_ground.png"))
+    {
+        std::cout << "Failed to load stone tile texture.\n";
+        return -1;
+    }
+
+    // Game loop
     while (window.isOpen())
     {
+        // Event polling
         sf::Event e;
         while (window.pollEvent(e))
         {
@@ -20,17 +53,47 @@ int main()
             }
         }
 
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
-            player.move(0.00f, -0.01f);
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
-            player.move(0.00f, 0.01f);
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
-            player.move(-0.01f, -0.00f);
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
-            player.move(0.01f, 0.00f);
+        // Camera movement
+        player.ProcessInput();
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Q))
+        {
+            system("clear");
+            mapGen.GenerateMap();
+            mapGen.PrintMap();
+            map = mapGen.GetMap();
+
+            for (int x=0; x<mapGen.GetWidth(); x++)
+            {
+                for (int y=0; y<mapGen.GetHeight(); y++)
+                {
+                    if (map[x][y] == 1)
+                    {
+                        sprites[x][y].setTexture(wall);
+                        sprites[x][y].setPosition(x*10, y*10);
+                    }
+                    else
+                    {
+                        sprites[x][y].setTexture(path);
+                        sprites[x][y].setPosition(x*10, y*10);
+                    }
+                }
+            }
+        }
+        window.setView(player.GetView());
 
         window.clear();
-        window.draw(player);
+
+        // Drawing
+        for (int x=0; x<mapGen.GetWidth(); x++)
+        {
+            for (int y=0; y<mapGen.GetHeight(); y++)
+            {
+                window.draw(sprites[x][y]);
+            }
+        }
+
+        window.draw(player.GetSprite());
         window.display();
     }
 
